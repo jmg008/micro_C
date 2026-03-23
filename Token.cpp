06043936cef38d5623f60115907d51e329e29682
jmg008 <jimingyeom@gmail.com>
2026-03-23 12:29:49 +0900

    [Feature] Token.h, Token.cpp 작성

---
+++ Token.cpp
@@ -0,0 +1,87 @@
+#include <map>
+#include <iomanip>
+#include "Token.h"
+
+using std::map;
+using std::setw;
+using std::left;
+
+static map<string, Kind> stringToKind = {
+    {"#Unknown",    Kind::Unknown},
+    {"#EndOfToken", Kind::EndOfToken},
+    
+    {"null",        Kind::NullLiteral},
+    {"#Number",     Kind::NullLiteral},
+    {"#String",     Kind::StringLiteral},
+    {"#Identifier", Kind::Identifier},
+
+    {"Int",         Kind::Int},
+    {"Char",        Kind::Char},
+    {"Void",        Kind::Void},
+    {"Struct",      Kind::Struct},
+
+    {"function",    Kind::Function},
+    {"return",      Kind::Return},
+    {"for",         Kind::For},
+    {"break",       Kind::Break},
+    {"continue",    Kind::Continue},
+    {"if",          Kind::If},
+    {"else",        Kind::Else},
+    {"print",       Kind::Print},
+    {"printLine",   Kind::PrintLine},
+
+    {"&&",          Kind::LogicalAnd},
+    {"||",          Kind::LogicalOr},
+    {"!",           Kind::LogicalNot},
+
+    {"=",           Kind::Assignment},
+
+    {"+",           Kind::Add},
+    {"-",           Kind::Subtract},
+    {"*",           Kind::Asterisk},
+    {"/",           Kind::Divide},
+    {"%",           Kind::Modulo},
+
+    {"==",          Kind::Equal},
+    {"!=",          Kind::NotEqual},
+    {"<",           Kind::LessThan},
+    {">",           Kind::GreaterThan},
+    {"<=",          Kind::LessOrEqual},
+    {">=",          Kind::GreaterOrEqual},
+
+    {".",           Kind::Dot},
+    {"->",          Kind::Arrow},
+
+    {",",           Kind::Comma},
+    {":",           Kind::Colon},
+    {";",           Kind::Semicolon},
+    {"(",           Kind::LeftParen},
+    {")",           Kind::RightParen},
+    {"{",           Kind::LeftBrace},
+    {"}",           Kind::RightBrace},
+    {"[",           Kind::LeftBraket},
+    {"]",           Kind::RightBraket},
+};
+
+static auto KindtoString = [] {
+    map<Kind, string> result;
+    for (auto& [key, value] : stringToKind)
+        result[value] = key;
+    return result;
+}();
+
+auto toKind(string string)->Kind {
+    if (stringToKind.count(string))
+        return stringToKind.at(string);
+    return Kind::Unknown;
+}
+
+auto toString(Kind type)->string {
+    if (KindtoString.count(type))
+        return KindtoString.at(type);
+    return "";
+}
+
+auto operator<<(ostream& stream, Token& token)->ostream& {
+    return stream << setw(12) << left << toString(token.kind) << token.string;
+}
---
+++ Token.h
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::ostream;
+
+enum class Kind {
+    Unknown, EndOfToken,
+
+    NullLiteral,
+    NumberLiteral, StringLiteral,
+    Identifier,
+
+    Int, Char, Void,
+    Struct,
+
+    Function, Return,
+    For, Break, Continue,
+    If, Else,
+    Print, PrintLine,
+
+    LogicalAnd, LogicalOr, LogicalNot,
+    Assignment,
+    Add, Subtract,
+    Asterisk, Divide, Modulo,
+    Equal, NotEqual,
+    LessThan, GreaterThan,
+    LessOrEqual, GreaterOrEqual,
+
+    Dot,
+    Arrow,
+
+    Comma, Colon, Semicolon,
+    LeftParen, RightParen,
+    LeftBrace, RightBrace,
+    LeftBraket, RightBraket,
+};
+
+auto toKind(string)->Kind;
+auto toString(Kind)->string;
+
+struct Token {
+    Kind kind = Kind::Unknown;
+    string string;
+};
+
+auto operator<<(ostream&, Token&)->ostream&;

5f59fd8d079aef647099c7495af3c11fb8ffd7ff
jmg008 <jimingyeom@gmail.com>
2026-04-13 12:23:05 +0900

    [Remove/Feature] Parser 부분 코드 전체 제거 -> 캡슐화로 재시작

--- Parser.cpp
+++ Parser.cpp
@@ -1,32 +1,25 @@
 #include <set>
 #include <iostream>
 #include <vector>
+#include "Parser.h"
 #include "Token.h"
 #include "Node.h"
 
 using std::vector;
 
-auto parse(vector<Token> tokens)->Program* {
+auto Parser::parse()->Program* {
     auto result = new Program();
     vector<Token>::iterator current = tokens.begin();
     while (current->kind != Kind::EndOfToken) {
         switch (current->kind) {
-        case Kind::Function: {
-            parseFunction(current);
-        }
+        
         default:
             break;
         }
     }
 }
 
-auto parseFunction(vector<Token>::iterator& current)->Function* {
-    auto result = new Function();
 
-    return result;
-}
-
-
-auto skipCurrent(vector<Token>::iterator& current)->void {
+auto Parser::skipCurrent()->void {
     current++;
 }
